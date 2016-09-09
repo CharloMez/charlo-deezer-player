3ed223f3938b1a6630d770b6a7e57eb70458d39f
Merlok <charly.mezari@gmail.com>
2016-09-09 19:02:38 +0200

    bind events

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 (function() {
     document.getElementById('playlist_button').onclick(Project.load('playlist', Project.loadPlaylist));
+    document.getElementById('login').onclick(Project.loginDeezer());
 })();
